e514f99e36cd7517110ace2fa1a3c26d9e68f8da
ingielek <ingielewicz.p@gmail.com>
2018-11-09 06:43:26 +0100

    kickoff

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   $('#openMenu').click(function(){
+       $('#main').fadeIn(200);
+       $('#openMenu').animate({left: '-10%'});
+       $('#closeMenu').animate({left: '95%'});
+       $('.menu').animate({left:'-1%'})
+   });
+
+
+    $('#closeMenu').click(function(){
+        $('#main').fadeOut(200);
+        $('#openMenu').animate({left: '0%'});
+        $('#closeMenu').animate({left: '100%'});
+        $('.menu').animate({left:'-50%'})
+    });
+
+});
